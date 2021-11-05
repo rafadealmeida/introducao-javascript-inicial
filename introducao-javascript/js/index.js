64bc3b77eeeacd4c760ed60c711b79137a9a625a
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-05 17:04:35 -0300

    Update index.js

--- introducao-javascript/js/index.js
+++ introducao-javascript/js/index.js
@@ -1,3 +1,14 @@
 var titulo = document.querySelector(".titulo");
 			
 titulo.textContent = "Nutrição Desaparecida"
+
+
+var peso =Number( document.querySelector(".info-peso"));
+var altura = Number(document.querySelector(".info-altura"));
+var imc =(document.querySelector(".info-imc"));
+
+var calculoImc = (peso * (altura**2));
+
+
+
+imc.textContent = calculoImc;
