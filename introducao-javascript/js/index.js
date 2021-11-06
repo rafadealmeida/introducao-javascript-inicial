7a6754aba6e9980d74e3f615817496080135cf97
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-05 23:45:15 -0300

    Update index.js

--- introducao-javascript/js/index.js
+++ introducao-javascript/js/index.js
@@ -3,43 +3,29 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Nutrição Desaparecida"
 
 
-var paciente2 = ( document.querySelector("#paciente_2"));
+function calculaImc(id,classPeso, classAltura,classImc) {
+    var paciente =(document.querySelector(id));
 
-var tdPeso = paciente2.querySelector(".info_peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = paciente2.querySelector(".info_altura")
-var altura = tdAltura.textContent;
-
-var tdImc = paciente2.querySelector (".info_imc");
-
-
-var imc =   peso / (altura ** 2);
-
- var imc = imc.toFixed(2)
-
-tdImc.textContent = imc
-
-function calculaImc(id,classPeso, classAltura,ClassImc) {
-    var paciente =(document.querySelector("#id"));
-
-    var tdPeso = paciente.querySelector(".classPeso");
-    var peso = tdPeso.textContent;
+    var peso = paciente.querySelector(classPeso).textContent;;
     
-    var tdAltura = paciente.querySelector(".classAltura")
-    var altura = tdAltura.textContent;
+    var altura = paciente.querySelector(classAltura).textContent;
     
-    var tdImc = paciente.querySelector (".classImc");
+    var tdImc = paciente.querySelector(classImc);
     
     
     var imc =   peso / (altura ** 2);
     
-     var imc = imc.toFixed(2)
+    var imc = imc.toFixed(2)
     
-    tdImc.textContent = imc
+    tdImc.textContent = imc;
+    
 }
-calculaImc(paciente_1,info-peso,info-altura,info-imc);
 
+var totalPacientes = 5
+for( i = 1; i <= totalPacientes; i++){
 
-console.log(imc)
+    calculaImc(`#paciente_${i}`,'.info_peso','.info_altura','.info_imc');
+
+}
+
 
