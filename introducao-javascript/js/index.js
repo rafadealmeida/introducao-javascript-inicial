afaf17165808b373f46ba9eb6d88a11da1af67f2
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-08 19:44:55 -0300

    Update index.js

--- introducao-javascript/js/index.js
+++ introducao-javascript/js/index.js
@@ -4,6 +4,10 @@ titulo.textContent = "Nutrição Desaparecida"
 
 
 function calculaImc(id,classPeso, classAltura,classImc) {
+    
+    var pesoValido = true;
+    var alturaValida = true;
+    
     var paciente =(document.querySelector(id));
 
     var peso = paciente.querySelector(classPeso).textContent;;
@@ -11,13 +15,25 @@ function calculaImc(id,classPeso, classAltura,classImc) {
     var altura = paciente.querySelector(classAltura).textContent;
     
     var tdImc = paciente.querySelector(classImc);
+
+    if(peso<0 || peso >100){
+        tdImc.textContent = "Inválido";
+        pesoValido = false;
+    }
+    if(altura<0 || altura > 3.0){
+        tdImc.textContent = "Inválido";
+        alturaValida = false
+    }
     
-    
-    var imc =   peso / (altura ** 2);
-    
-    var imc = imc.toFixed(2)
-    
-    tdImc.textContent = imc;
+
+    if(pesoValido && alturaValida){
+
+        var imc =   peso / (altura ** 2);
+        
+        var imc = imc.toFixed(2)
+        
+        tdImc.textContent = imc;
+    }
     
 }
 
