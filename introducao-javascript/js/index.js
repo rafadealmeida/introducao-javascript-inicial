e913736762fb9fc9bdb91b46be849e0712935463
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-09 15:44:55 -0300

    Update index.js

--- introducao-javascript/js/index.js
+++ introducao-javascript/js/index.js
@@ -51,41 +51,4 @@ for( i = 1; i <= pacientes.length; i++){
     calculaImc(`#paciente_${i}`,'.info_peso','.info_altura','.info_imc');
 
 }
-
-var botao = document.querySelector("#adicionar-paciente");
-
-botao.addEventListener("click", function(evento){
-    evento.preventDefault();
-
-    var formulario = document.querySelector("#form-adiciona");
-
-    var nome = formulario.nome.value;        
-    var altura = formulario.altura.value;        
-    var peso = formulario.peso.value;        
-    var gordura = formulario.gordura.value;        
-     
-    var pacienteAdd = document.createElement("tr");
-    
-    var nomeAdd = document.createElement("td");
-    var pesoAdd = document.createElement("td");
-    var alturaAdd = document.createElement("td");
-    var gorduraAdd = document.createElement("td");
-    var imcAdd = document.createElement("td");
-
-    nomeAdd.textContent = nome;     
-    pesoAdd.textContent = peso;
-    alturaAdd.textContent = altura;
-    gorduraAdd.textContent = gordura;
-
-    pacienteAdd.appendChild(nomeAdd)
-    pacienteAdd.appendChild(pesoAdd)
-    pacienteAdd.appendChild(alturaAdd)
-    pacienteAdd.appendChild(gorduraAdd)
-    
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteAdd);
-
-
-
-})
+ 
