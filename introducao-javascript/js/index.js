4dd83f43cdb674fccc665457be127906b789486a
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-09 14:10:18 -0300

    Update index.js

--- introducao-javascript/js/index.js
+++ introducao-javascript/js/index.js
@@ -46,7 +46,7 @@ function calculaImc(id,classPeso, classAltura,classImc) {
 var pacientes = document.querySelectorAll('.paciente');
 
 
-for( i = 1; i < pacientes.length; i++){
+for( i = 1; i <= pacientes.length; i++){
 
     calculaImc(`#paciente_${i}`,'.info_peso','.info_altura','.info_imc');
 
@@ -63,6 +63,38 @@ function mostraMensagem() {
 
 var botaoAdicionar = document.querySelector ('#adicionar-paciente');
 
-botaoAdicionar.addEventListener('click', function(){
-    alert('Você clicou!')
+botaoAdicionar.addEventListener('click', function(evento){
+    evento.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+
+    var pacienteTr= document.createElement("tr")
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    
+    var tabela = document.querySelector ("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+
 })
