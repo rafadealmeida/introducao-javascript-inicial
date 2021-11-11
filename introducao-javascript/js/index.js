82ae8c06d62547e1f0b098a2ec0c676b80203f85
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-11 11:57:28 -0300

    Update index.js

--- introducao-javascript/js/index.js
+++ introducao-javascript/js/index.js
@@ -16,11 +16,11 @@ function calculaImc(id,classPeso, classAltura,classImc) {
     
     var tdImc = paciente.querySelector(classImc);
 
-    if(i%2==0){
+   /* if(i%2==0){
 
         paciente.classList.add("linhaPar")
         
-    }
+    }*/
 
     if(peso <= 0 || peso >= 1000){
         tdImc.textContent = "Peso inválido";
