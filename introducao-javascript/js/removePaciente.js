d37447bbe9b3ae0a5adbc421420ee5234dd5bafa
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-15 15:41:57 -0300

    Implementação de remoção de pacinetes

---
+++ introducao-javascript/js/removePaciente.js
@@ -0,0 +1,12 @@
+var tabela = document.querySelector('#tabela-pacientes');
+
+tabela.addEventListener('dblclick',function(event) {
+    var alvoDoEvento = event.target;//verificando aonde o double click foi, que pode ser em qualquer td da tabela.
+    var paiDoAlvo = alvoDoEvento.parentNode; //com o '.parentNode' pego a partir do click o pai do alvo, que no caso seria a linha da tabela.
+
+     var resposta = prompt('Você deseja apagar esse paciente? Responda com sim ou nao.')
+
+     if (resposta == "sim"){
+         paiDoAlvo.remove();
+     }
+})
